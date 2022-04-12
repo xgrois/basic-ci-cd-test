b3d6adc4c1672342ef52f6de02593b9669a7c3c6
xgrois <xgrois@gmail.com>
2022-04-12 12:24:35 +0200

    Add webapi and xunit tests

---
+++ Dummy.Api.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Dummy.Api.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 0);
+        }
+    }
+}
